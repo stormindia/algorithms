2a6c6a0774549f05b5ec0967bc683fb086023e96
Harshit Bajpai <bajpaiharshit14@gmail.com>
2019-08-18 22:27:53 +0530

    percolation problem completed, edge cases fixed, dynamically tells if system percolates

--- week1/percolation.py
+++ week1/percolation.py
@@ -12,8 +12,8 @@ class percolation:
         # initially tree size of all elements is 1 execept v_top and v_bottom so that root always points towards them
         self.grid = [[i,i, False, 1] for i in range(self.grid_size)]
         #print(self.grid[0][3])
-        self.v_top = [-1,-1, True, 1000]
-        self.v_bottom = [-2,-2,True,1000]
+        self.v_top = [-1,-1, True, 1]
+        self.v_bottom = [-2,-2,True,1]
 
 
     def root_value(self, k):
@@ -29,6 +29,11 @@ class percolation:
 
         while(k != self.grid[k][0]):
             k = self.grid[k][0]
+            if (k == -1):
+                return self.v_top[0]
+            if (k == -2):
+                return self.v_bottom[0]
+
 
         return k
 
@@ -43,24 +48,46 @@ class percolation:
         #topmost row
         if (a >=0 and a < self.N):
             list.append(self.v_top)
+            if(self.grid[a+self.N][2] is True):
+                list.append(self.grid[a+self.N])
+            if(a != self.N and self.grid[a+1][2] is True):
+                list.append(self.grid[a+1])
+            if(a != 0 and self.grid[a-1][2] is True):
+                list.append(self.grid[a-1])
             border_condition = True
 
         #bottom most row
         if (a >= (self.N -1)*self.N and a < self.grid_size):
             list.append(self.v_bottom)
+            if(self.grid[a-self.N][2] is True):
+                list.append(self.grid[a-self.N])
+            if(a != self.grid_size -1 and self.grid[a+1][2] is True):
+                list.append(self.grid[a+1])
+            if(a != (self.N -1)*self.N and self.grid[a-1][2] is True):
+                list.append(self.grid[a-1])
             border_condition = True
 
         #leftmost column
-        if (a % self.N == 0):
+        if (a % self.N == 0 and a != 0 and a != (self.N -1)*self.N):
+            #print(a)
             if(self.grid[a+1][2] is True):
                 list.append(self.grid[a+1])
-                border_condition = True
+            if(self.grid[a+self.N][2] is True):
+                list.append(self.grid[a+self.N])
+            if(self.grid[a-self.N][2] is True):
+                list.append(self.grid[a-self.N])
+            #print(list)
+            border_condition = True
 
         #rightmost column
-        if((a+1) % self.N == 0):
+        if((a+1) % self.N == 0 and a != self.N and a != self.grid_size -1):
             if(self.grid[a-1][2] is True):
                 list.append(self.grid[a-1])
-                border_condition = True
+            if(self.grid[a+self.N][2] is True):
+                list.append(self.grid[a+self.N])
+            if(self.grid[a-self.N][2] is True):
+                list.append(self.grid[a-self.N])
+            border_condition = True
 
         #for in between elements
         if (border_condition is True):
@@ -99,15 +126,38 @@ class percolation:
         root_a = self.root_value(a)
         root_b = self.root_value(b)
 
+
         if(root_a == root_b):
             return
+        elif(root_a < 0 and root_b < 0):
+            #one of the element points to v_top and other points to v_bottom
+            print("system percolates now")
+            if(root_a == -2):
+                #connect a and v_bottom to v_top
+                self.v_bottom[0] = self.v_top[0]
+                self.grid[a][0] = self.v_top[0]
+                self.v_top[3] = self.v_top[3] + self.grid[a][3]
+            else:
+                self.v_top[0] = self.v_bottom[0]
+                self.grid[a][0] = self.v_bottom[0]
+                self.v_bottom[3] = self.v_bottom[3] + self.grid[a][3]
+        elif(root_b == -1):
+            if(self.v_top[3] >= self.grid[a][3]):
+                self.grid[a][0] = root_b
+                #increment the weight
+                self.v_top[3] = self.v_top[3] + self.grid[a][3]
+            else:
+                self.v_top[0] = root_a
+                self.grid[a][3] = self.grid[a][3] + self.v_top[3]
 
-        elif(b == -1):
-            self.grid[a][0] = root_b
-            self.v_top[3] = self.v_top[3] + self.grid[a][3]
-        elif(b == -2):
-            self.grid[a][0] = root_b
-            self.v_bottom[3] = self.v_bottom[3] + self.grid[a][3]
+        elif(root_b == -2):
+            if(self.v_bottom[3] >= self.grid[a][3]):
+                self.grid[a][0] = root_b
+                #increment the weight
+                self.v_bottom[3] = self.v_bottom[3] + self.grid[a][3]
+            else:
+                self.v_bottom[0] = root_a
+                self.grid[a][3] = self.grid[a][3] + self.v_bottom[3]
 
         elif(self.grid[a][3] >= self.grid[b][3]):
             self.grid[b][0] = root_a
@@ -128,13 +178,20 @@ class percolation:
             print("{} -> this element index out of bound open function".format(a))
             return
 
+        self.grid[a][2] = True
         open_elements = self.get_adjacent_open_elements(a)
-        print(open_elements)
         for i in range(len(open_elements)):
             self.union(a, open_elements[i][1])
 
+        #print(self.grid)
+        #print(self.v_top)
+        #print(self.v_bottom)
+
 
     def isPercolating(self):
+        # print(self.grid)
+        # print(self.v_top)
+        # print(self.v_bottom)
         if(self.v_top[0] == self.v_bottom[0]):
             print("yes")
             return True
@@ -145,9 +202,10 @@ class percolation:
 
 
 
-system_1 = percolation(2)
+system_1 = percolation(3)
 
 
 system_1.open(0)
-system_1.open(2)
+system_1.open(3)
+system_1.open(6)
 system_1.isPercolating()
