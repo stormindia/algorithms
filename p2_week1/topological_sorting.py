3b91c105963e08fe483a210c6eb9308e42f62771
stormindia <bajpaiharshit14@gmail.com>
2020-02-09 00:16:06 +0530

    completed topological sorting of DAG

--- p2_week1/topological_sorting.py
+++ p2_week1/topological_sorting.py
@@ -3,9 +3,34 @@
 # Run DFS
 # Return vertices in reverse post-order
 
+# to print vertices in reverse post order - use stack - Using just an array and prinitng it in reverse for simiplicity
+
 from digraph_api import Digraph
 
 class TopologicalSort:
 
     def __init__(self,graph):
-        
+        self.G = graph
+        self.marked = []*self.G.V
+        self.reversePost = []*self.G.V
+
+        for i in self.G.V:
+            if(self.marked[i] is not True):
+
+                self.dfs_for_topological_sort(self.G,i)
+
+
+    def dfs_for_topological_sort(self,graph,vertex):
+
+        self.marked[vertex] = True
+
+        for i in self.adj[vertex]:
+            if(self.marked[i] is not True):
+                self.dfs_for_topological_sort(self.G,i)
+        self.reversePost.append(vertex)
+
+    def return_sorted_graph(self):
+
+        # print the array in reverse order
+        for i in range(len(self.reversePost),0, -1):
+            print(self.reversePost[i])
