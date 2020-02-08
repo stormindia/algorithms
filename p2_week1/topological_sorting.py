95ec67278051ec0227f4060c79c55c9a121e4fe8
stormindia <bajpaiharshit14@gmail.com>
2020-02-09 00:02:38 +0530

    topological sort started

---
+++ p2_week1/topological_sorting.py
@@ -0,0 +1,11 @@
+# Works only on directed acyclic graph (dag)
+# Algorithm
+# Run DFS
+# Return vertices in reverse post-order
+
+from digraph_api import Digraph
+
+class TopologicalSort:
+
+    def __init__(self,graph):
+        
