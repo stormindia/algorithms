381cccde3f6ddb711754ff92c4601bd1688fec08
stormindia <bajpaiharshit14@gmail.com>
2020-04-30 08:22:10 +0530

    corrected pathTo function

--- p2_week2/directed_edge_weighted_graph_api.py
+++ p2_week2/directed_edge_weighted_graph_api.py
@@ -5,17 +5,14 @@ class DirectedEdge:
         self.v2 = v2
         self.weight = weight
 
-
     def edge_from(self):
         return self.v1
-
 
     def edge_to(self):
         return self.v2
 
     def weight(self):
         return self.weight
-
 
 
 #Allow self loop and parallel edges
@@ -45,3 +42,38 @@ class EdgeWeightedDigraph:
     #returns all the edges
     def edges(self):
         return self.edges
+
+
+#Refer https://www.coursera.org/learn/algorithms-part2/lecture/igCiL/shortest-path-properties
+#Part2 week2 shortest path properties
+#Shortest path tree APIS
+class SPT_apis:
+    def __init__(self, G, source_vertex):
+        self.V = G.V
+        self.s = source_vertex
+        self.distTo = []*self.V
+        self.edgeTo = []*self.V #REFER README
+
+
+    def distTo(self,v):
+        return self.distTo[v]
+
+    def pathTo(self,v):
+        path = []
+
+        while(e != NULL):
+            k = edgeTo[v]
+            e = edgeTo[k.edge_from()]
+            path.append(e)
+
+        return path
+
+    def relax(self,e):
+        v1 = e.edge_from()
+        v2 = e.edge_to()
+
+        if(self.distTo[w] > self.distTo[v] + e.weight()):
+            self.distTo[w] = self.distTo[v] + e.weight()
+            self.edgeTo[w] = e
+
+        return
