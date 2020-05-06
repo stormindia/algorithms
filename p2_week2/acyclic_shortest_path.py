0fa0a9d37cbaf6062521e347e995261317ee2451
stormindia <bajpaiharshit14@gmail.com>
2020-05-06 18:06:52 +0530

    shortest path in Edge weighted DAGs

---
+++ p2_week2/acyclic_shortest_path.py
@@ -0,0 +1,44 @@
+from ../p2_week1/topological_sorting import TopologicalSort
+
+from directed_edge_weighted_graph_api import *
+
+
+MAX = 99999
+
+G = EdgeWeightedDigraph(V)
+
+class Acyclic_SP:
+
+    def __init__(self,G,source_vertex):
+        self.G = G
+        self.S = source_vertex
+        self.distTo = [MAX]*self.G.V
+        self.edgeTo = []*self.G.V
+
+        topological_graph = TopologicalSort(self.G)
+        topological_ordered_verteices = topological_graph.reverse_post_order()
+
+        for vertex in topological_ordered_verteices:
+            for edge in self.G.adjacent_edges[vertex]:
+                self.relax(edge)
+
+
+    def relax(self,e):
+        v1 = e.edge_from()
+        v2 = e.edge_to()
+
+        if(self.distTo[v2] is NULL):
+            if(v1 is self.s):
+                self.distTo[v2] = e.weight()
+                self.edgeTo[v2] = e
+            else:
+                self.distTo[v2] = self.distTo[v1] + e.weight()
+                self.edgeTo[v2] = e
+        else:
+            if(self.distTo[v2] > self.distTo[v1] + e.weight()):
+                self.distTo[v2] = self.distTo[v1] + e.weight()
+                self.edgeTo[v2] = e
+            else:
+                pass
+
+        return
