f5dcb2fa9c95039a0fb212d823c42d1393eb77cc
stormindia <bajpaiharshit14@gmail.com>
2020-05-13 00:30:35 +0530

    added shortest path for seam carving, min horizontal and min vertical seam; delete seam remaining

--- p2_week2/seam_carver.py
+++ p2_week2/seam_carver.py
@@ -5,14 +5,25 @@
 
 #Since we don't have a image data type as in the assignemnt, we assume that input is W*H array with each element having RGB as its value
 
-from directed_edge_weighted_graph_api import EdgeWeightedDigraph
 from acyclic_shortest_path import Acyclic_SP
+from ../p2_week1/topological_sorting import TopologicalSort
+from directed_edge_weighted_graph_api import *
+
 
 example_input =[[255,255,255],[255,255,255],[255,255,255],
                 [255,255,255],[255,255,255],[255,255,255],
                 [255,255,255],[255,255,255],[255,255,255]]
 
 
+
+# a 3 by 4 image
+#(0,0) (1,0) (2,0)
+#(0,1) (1,1) (2,1)
+#(0,2) (1,2) (2,2)
+#(0,3) (1,3) (2,3)
+
+MAX = 99999
+#################HELPER class#########################
 class Seam_CarverEdge:
 
     def __init__(self,v1,weight1,v2,weight2):
@@ -33,6 +44,82 @@ class Seam_CarverEdge:
     def weight_to(self):
         return self.weightV2
 
+########################################################################################
+
+################################Seam_Carver############################################
+
+class Acyclic_SP_for_SEAM:
+
+    def __init__(self,G,source_vertex, seam_direction):
+        self.G = G
+        self.S = source_vertex
+        self.distTo = [MAX]*self.G.V
+        self.edgeTo = []*self.G.V
+        self.seam_direction = seam_direction #horizontal or vertical
+        if(self.seam_direction != 'horizontal' or self.seam_direction != 'vertical'):
+            return 'invalid seam_direction'
+
+        topological_graph = TopologicalSort(self.G)
+        topological_ordered_verteices = topological_graph.return_sorted_graph()
+
+        for vertex in topological_ordered_verteices:
+            for edge in self.G.adjacent_edges[vertex]:
+                self.relax(edge)
+
+        # Return 3 things -
+        # 1. vertex in the bottom row(or column) for which seam is min
+        # 2. total seam  value
+        # 3. path from source_vertex to the vertex found in step 1
+        if(self.seam_direction == 'horizontal'):
+            MIN = MAX
+            for j in range(self.h - 1):
+                if(self.distTo[(self.w -1) + j*(self.h-1)] == MAX): #vertex not touched
+                    pass
+                else:
+                    if(self.distTo[(self.w -1) + j*(self.h-1)] < MIN):
+                        MIN = self.distTo[(self.w -1) + j*(self.h-1)]
+                        vertex_pos = (self.w - 1) + j*(self.h-1)
+        else if(self.seam_direction == 'vertical'):
+            MIN = MAX
+            for i in range(self.w - 1):
+                if(self.distTo[i*(self.w -1) + (self.h-1)] == MAX): #vertex not touched
+                    pass
+                else:
+                    if(self.distTo[i*(self.w -1) + (self.h-1)] < MIN):
+                        MIN = self.distTo[i*(self.w -1) + (self.h-1)]
+                        vertex_pos = i*(self.w - 1) + (self.h-1)
+
+
+        seam_value = MIN
+
+        i = abs((vertex_pos - (self.h - 1))) % self.w
+        j = abs((vertex_pos - (self.w -1))) % self.h
+
+        self.vertex = self.vertices[i][j] #vertex is the vertex for which the seam is min
+        curr_vertex = vertex
+        self.path_arr = []
+        while(curr_vertex != source_vertex):
+            self.path_arr.append(curr_vertex)
+            curr_vertex = self.edgeTo[vertex_pos].from()
+        self.path_arr.append(source_vertex)
+
+    def relax(self,e):
+        v1 = e.edge_from()
+        v2 = e.edge_to()
+        v1_pos = v1[0] + v1[1]*(self.h - 1)
+        v2_pos = v2[0] + v2[1]*(self.h -1)
+
+        if(self.distTo[v2_pos] > self.distTo[v1_pos] + e.weight_from()):
+            self.distTo[v2_pos] = self.distTo[v1_pos] + e.weight_from()
+            self.edgeTo[v2_pos] = e
+        else:
+            pass
+
+        return
+
+    def return_min_seam(self):
+        return [seam_value, vertex, path_arr]
+
 
 class Seam_Carver:
 
@@ -52,12 +139,7 @@ class Seam_Carver:
                 k += 1
             self.vertices.append(col)
 
-    def width(self):
-        return self.w
-
-    def height(self):
-        return self.h
-
+##################### HELPER FUNCTIONS ###########################
     def isBorderElement(self,x,y):
         if(x == 0 or x == self.w - 1 or y == 0 or y == self.h -1):
             return True
@@ -68,7 +150,13 @@ class Seam_Carver:
             return False
 
         return True
+#################################################################
 
+    def width(self):
+        return self.w
+
+    def height(self):
+        return self.h
 
     def calculate_energy(self,x,y):
 
@@ -132,3 +220,37 @@ class Seam_Carver:
                     G_horizontal.add_edge(edge)
 
             return G_horizontal
+
+    def findHorizontalSeam(self):
+
+        self.G_horizontal = self.create_horizontal_graph(self.G_horizontal)
+        min_seam = []
+        for i in range(self.h -1):
+            calculate_seam = Acyclic_SP_for_SEAM(self.G_horizontal, self.vertices[0][i], 'horizontal')
+            seam = calculate_seam.return_min_seam()
+            if(min_seam == []):
+                min_seam = seam
+            else:
+                if(min_seam[0] > seam[0]):
+                    min_seam = seam
+                else:
+                    pass
+
+        return min_seam
+
+    def findVerticalSeam(self):
+
+        self.G_vertical = self.create_vertical_graph(self.G_vertical)
+        min_seam = []
+        for j in range(self.w -1):
+            calculate_seam = Acyclic_SP_for_SEAM(self.G_vertical, self.vertices[j][0], 'vertical')
+            seam = calculate_seam.return_min_seam()
+            if(min_seam == []):
+                min_seam = seam
+            else:
+                if(min_seam[0] > seam[0]):
+                    min_seam = seam
+                else:
+                    pass
+
+        return min_seam
