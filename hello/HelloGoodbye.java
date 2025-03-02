16ac0d7a95cb3b3c7ebc2941e23f518a679fec99
Harshit-Bajpai <Harshit.Bajpai@skyscanner.net>
2025-03-02 20:46:37 +0000

    fix  test

--- hello/HelloGoodbye.java
+++ hello/HelloGoodbye.java
@@ -1,7 +1,6 @@
 public class HelloGoodbye {
     public static void main(String[] args) {
-        // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello " + args[0] + " and " + args[1]);
-        System.out.println("Goodbye " + args[1] + " and " + args[0]);
+        System.out.println("Hello " + args[0] + " and " + args[1] + ".");
+        System.out.println("Goodbye " + args[1] + " and " + args[0] + ".");
     }
 }
